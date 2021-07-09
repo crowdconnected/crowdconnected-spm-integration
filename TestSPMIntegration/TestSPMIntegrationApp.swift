647b5e56096b4bcb82ba9fed67bf16b29b772288
Robert Tanase <robert.tanase97@gmail.com>
2021-07-09 11:26:46 +0300

    Initial Commit

---
+++ TestSPMIntegration/TestSPMIntegrationApp.swift
@@ -0,0 +1,12 @@
+//  Created by TCode on 9/7/21.
+
+import SwiftUI
+
+@main
+struct TestSPMIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
