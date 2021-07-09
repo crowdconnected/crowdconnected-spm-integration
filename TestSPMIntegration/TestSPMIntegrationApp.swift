47c86bb4f38305328822c7e0a149a8e2a9b1bd1c
Robert Tanase <robert.tanase97@gmail.com>
2021-07-09 19:12:25 +0300

    Integrate CrowdConnected SDK via SPM

--- TestSPMIntegration/TestSPMIntegrationApp.swift
+++ TestSPMIntegration/TestSPMIntegrationApp.swift
@@ -1,12 +1,41 @@
 //  Created by TCode on 9/7/21.
 
 import SwiftUI
+import CoreLocation
+import CrowdConnectedCore
+import CrowdConnectedIPS
+import CrowdConnectedShared
 
 @main
 struct TestSPMIntegrationApp: App {
+
+    let locationsProvider = LocationsProvider()
+    let locationManager = CLLocationManager()
+
+    init() {
+
+        CrowdConnectedIPS.activate()
+        CrowdConnected.shared.start(appKey: "appkey", token: "iosuser", secret: "Ea80e182$") { deviceId, error in
+            guard let id = deviceId else {
+                return
+            }
+
+            print(id)
+        }
+
+        CrowdConnected.shared.delegate = locationsProvider
+        locationManager.requestWhenInUseAuthorization()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class LocationsProvider: CrowdConnectedDelegate {
+    func didUpdateLocation(_ locations: [Location]) {
+        print("Got new location update")
     }
 }
